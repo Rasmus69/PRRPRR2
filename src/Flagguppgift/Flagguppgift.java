bbabb53f5ef627e363cbd5fa4e952cf5dd62325d
Rasmus69 <Rasmus0526@gmail.com>
2019-09-27 14:14:55 +0200

    Andra

--- src/Flagguppgift/Flagguppgift.java
+++ src/Flagguppgift/Flagguppgift.java
@@ -3,13 +3,20 @@ package Flagguppgift;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Flagguppgift extends Application{
+        static BorderPane main;
         public static void main(String[] args){
             launch(args);
 
@@ -22,8 +29,12 @@ public class Flagguppgift extends Application{
 
             Group root = new Group();
 
+            main = new BorderPane();
+            root.getChildren().add(main);
+
+
             HBox buttonPane = new HBox();
-            root.getChildren().add(buttonPane);
+            main.setTop(buttonPane);
 
             Button[] buttons = {
                     new Button("Sverige"),
@@ -33,28 +44,64 @@ public class Flagguppgift extends Application{
             };
 
             buttonPane.getChildren().addAll(buttons);
-
             for (int i = 0; i < buttons.length; i++) {
-                buttons[i].setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent actionEvent) {
-                        if (i == 0){
+
+                if (i == 0) {
+                    buttons[i].setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent actionEvent) {
                             renderFlag("Sverige");
-                        }else if (i == 1){
+                        }
+                    });
+                } else if (i == 1) {
+                    buttons[i].setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent actionEvent) {
                             renderFlag("Norge");
                         }
-                    }
-                });
+                    });
+                }
             }
 
-            Scene scene = new Scene(root, 1200, 600);
+            Scene scene = new Scene(root, 800, 500);
             primaryStage.setTitle("Flagspel");
             primaryStage.setScene(scene);
             primaryStage.show();
         }
 
         public static void renderFlag(String flag){
+            BorderPane con = new BorderPane();
+            StackPane container = new StackPane();
+            container.setAlignment(Pos.CENTER);
 
+            if(flag == "Sverige"){
+
+                Rectangle r = new Rectangle();
+                r.setWidth(100);
+                r.setHeight(50);
+                r.setFill(Color.BLUE);
+
+                Rectangle r2 = new Rectangle();
+                r2.setWidth(10);
+                r2.setHeight(50);
+                r2.setFill(Color.YELLOW);
+                r2.setTranslateX(-15);
+
+                Rectangle r3 = new Rectangle();
+                r3.setWidth(100);
+                r3.setHeight(10);
+                r3.setFill(Color.YELLOW);
+
+                container.getChildren().add(r);
+                container.getChildren().add(r2);
+                container.getChildren().add(r3);
+
+            }else if(flag == "Norge"){
+
+            }
+
+            con.setCenter(container);
+            main.setCenter(con);
         }
     }
 
